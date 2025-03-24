fe0d367a8466ee972f0426b4e3ab55255cdbabf1
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2025-03-24 12:05:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: "mongodb+srv://xaayux:909090xd@cluster0.mojpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    MONGODB_DB: "gumasta_krishi_kendra"
+  }
+}
 
+module.exports = nextConfig
